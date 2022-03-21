7048453e29c5999b83438fb4fbf5ae2d47f1bd33
esimov <esimovaq@yahoo.com>
2022-03-21 14:57:16 +0200

    Removed reduce.go

--- map.go
+++ map.go
@@ -13,8 +13,10 @@ func main() {
 
 func Map[T1, T2 any](s []T1, fn func(T1) T2) []T2 {
 	newSlice := make([]T2, len(s))
+
 	for idx, v := range s {
 		newSlice[idx] = fn(v)
 	}
+
 	return newSlice
 }
