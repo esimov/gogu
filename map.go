31bad9a75f4896a0fff6482456782c326a979df1
esimov <esimovaq@yahoo.com>
2022-03-26 06:32:28 +0200

    Added map Keys & Values methods

--- map.go
+++ map.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	mapVals := map[int]int64{1: 1, 2: 2, 3: 3}
+	mapKeys := map[string]int{"a": 1, "b": 2, "c": 3}
 
 	fmt.Println("==================MapValues")
 	newVals := MapValues[int, int64, string](mapVals, func(v int64) string {
@@ -16,7 +17,6 @@ func main() {
 	fmt.Println(newVals)
 
 	fmt.Println("==================MapKeys")
-	mapKeys := map[string]int{"a": 1, "b": 2, "c": 3}
 	newKeys := MapKeys[string, int, string](mapKeys, func(k string, v int) string {
 		return k + strconv.Itoa(v)
 	})
@@ -29,6 +29,12 @@ func main() {
 		return v < 10
 	})
 	fmt.Println(res2)
+
+	fmt.Println("==================Keys")
+	fmt.Println(Keys(mapKeys))
+
+	fmt.Println("==================Values")
+	fmt.Println(Values(mapKeys))
 
 	fmt.Println("==================Filter2DMap")
 	users := []map[string]map[string]int{
@@ -53,11 +59,35 @@ func main() {
 	fmt.Println(res3)
 }
 
+func Keys[K comparable, V any](s map[K]V) []K {
+	keys := make([]K, len(s))
+
+	idx := 0
+	for k, _ := range s {
+		keys[idx] = k
+		idx++
+	}
+
+	return keys
+}
+
+func Values[K comparable, V any](s map[K]V) []V {
+	values := make([]V, len(s))
+
+	idx := 0
+	for _, v := range s {
+		values[idx] = v
+		idx++
+	}
+
+	return values
+}
+
 func MapValues[K comparable, V, R any](s map[K]V, fn func(V) R) map[K]R {
 	newMap := map[K]R{}
 
-	for idx, v := range s {
-		newMap[idx] = fn(v)
+	for k, v := range s {
+		newMap[k] = fn(v)
 	}
 
 	return newMap
@@ -73,8 +103,8 @@ func MapKeys[K comparable, V any, R comparable](s map[K]V, fn func(K, V) R) map[
 	return newMap
 }
 
-func FilterMap[T1 comparable, T2 any](s []map[T1]T2, fn func(T2) bool) []map[T1]T2 {
-	filtered := []map[T1]T2{}
+func FilterMap[K comparable, V any](s []map[K]V, fn func(V) bool) []map[K]V {
+	filtered := []map[K]V{}
 
 	for _, v := range s {
 		for _, v1 := range v {
@@ -87,8 +117,8 @@ func FilterMap[T1 comparable, T2 any](s []map[T1]T2, fn func(T2) bool) []map[T1]
 	return filtered
 }
 
-func Filter2DMap[T1 comparable, T2 any](s []map[T1]map[T1]T2, fn func(map[T1]T2) bool) []map[T1]map[T1]T2 {
-	filtered := []map[T1]map[T1]T2{}
+func Filter2DMap[K comparable, V any](s []map[K]map[K]V, fn func(map[K]V) bool) []map[K]map[K]V {
+	filtered := []map[K]map[K]V{}
 
 	for _, v := range s {
 		for _, v1 := range v {
