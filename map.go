3732023b4c0ff915a575f4e98dd1644c5251abd4
esimov <esimovaq@yahoo.com>
2022-03-26 10:56:33 +0200

    Extended the map with new methods

--- map.go
+++ map.go
@@ -9,6 +9,12 @@ func main() {
 	mapVals := map[int]int64{1: 1, 2: 2, 3: 3}
 	mapKeys := map[string]int{"a": 1, "b": 2, "c": 3}
 
+	fmt.Println("==================Keys")
+	fmt.Println(Keys(mapKeys))
+
+	fmt.Println("==================Values")
+	fmt.Println(Values(mapKeys))
+
 	fmt.Println("==================MapValues")
 	newVals := MapValues[int, int64, string](mapVals, func(v int64) string {
 		v = v * 10
@@ -22,22 +28,39 @@ func main() {
 	})
 	fmt.Println(newKeys)
 
-	fmt.Println("==================FilterMap")
-	mp := []map[string]int{{"user": 1}}
+	mp := map[int]string{1: "John", 2: "Doe", 3: "Fred"}
 
-	res2 := FilterMap[string, int](mp, func(v int) bool {
-		return v < 10
+	fmt.Println("==================FindKey")
+	res1 := FindKey[int, string](mp, func(v string) bool {
+		return v == "John"
+	})
+	fmt.Println(res1)
+
+	fmt.Println("==================FilterMap")
+	res2 := FilterMap[int, string](mp, func(v string) bool {
+		return v == "John"
 	})
 	fmt.Println(res2)
 
-	fmt.Println("==================Keys")
-	fmt.Println(Keys(mapKeys))
+	usersMap := map[string]map[string]int{
+		"bernie": {
+			"age":     30,
+			"ranking": 1,
+		},
 
-	fmt.Println("==================Values")
-	fmt.Println(Values(mapKeys))
+		"robert": {
+			"age":     20,
+			"ranking": 5,
+		},
+	}
 
 	fmt.Println("==================Filter2DMap")
-	users := []map[string]map[string]int{
+	res3 := Filter2DMap[string, int](usersMap, func(v int) bool {
+		return v > 20
+	})
+	fmt.Println(res3)
+
+	usersSlice := []map[string]map[string]int{
 		{
 			"bernie": {
 				"age":     30,
@@ -52,18 +75,18 @@ func main() {
 		},
 	}
 
-	fmt.Println(users)
-	res3 := Filter2DMap[string, int](users, func(v map[string]int) bool {
-		return v["age"] >= 10
+	fmt.Println("==================Filter2DMapSlice")
+	res4 := Filter2DMapSlice[string, int](usersSlice, func(v map[string]int) bool {
+		return v["age"] > 20 && v["ranking"] < 5
 	})
-	fmt.Println(res3)
+	fmt.Println(res4)
 }
 
-func Keys[K comparable, V any](s map[K]V) []K {
-	keys := make([]K, len(s))
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, len(m))
 
 	idx := 0
-	for k, _ := range s {
+	for k, _ := range m {
 		keys[idx] = k
 		idx++
 	}
@@ -71,11 +94,11 @@ func Keys[K comparable, V any](s map[K]V) []K {
 	return keys
 }
 
-func Values[K comparable, V any](s map[K]V) []V {
-	values := make([]V, len(s))
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, len(m))
 
 	idx := 0
-	for _, v := range s {
+	for _, v := range m {
 		values[idx] = v
 		idx++
 	}
@@ -83,33 +106,56 @@ func Values[K comparable, V any](s map[K]V) []V {
 	return values
 }
 
-func MapValues[K comparable, V, R any](s map[K]V, fn func(V) R) map[K]R {
+func MapValues[K comparable, V, R any](m map[K]V, fn func(V) R) map[K]R {
 	newMap := map[K]R{}
 
-	for k, v := range s {
+	for k, v := range m {
 		newMap[k] = fn(v)
 	}
 
 	return newMap
 }
 
-func MapKeys[K comparable, V any, R comparable](s map[K]V, fn func(K, V) R) map[R]V {
+func MapKeys[K comparable, V any, R comparable](m map[K]V, fn func(K, V) R) map[R]V {
 	newMap := map[R]V{}
 
-	for k, v := range s {
+	for k, v := range m {
 		newMap[fn(k, v)] = v
 	}
 
 	return newMap
 }
 
-func FilterMap[K comparable, V any](s []map[K]V, fn func(V) bool) []map[K]V {
-	filtered := []map[K]V{}
+func FindKey[K comparable, V any](m map[K]V, fn func(V) bool) K {
+	var res K
+	for k, v := range m {
+		if fn(v) {
+			res = k
+			break
+		}
+	}
+	return res
+}
 
-	for _, v := range s {
+func FilterMap[K comparable, V any](m map[K]V, fn func(V) bool) map[K]V {
+	filtered := map[K]V{}
+
+	for k, v := range m {
+		if fn(v) {
+			filtered[k] = v
+		}
+	}
+
+	return filtered
+}
+
+func Filter2DMap[K comparable, V any](m map[K]map[K]V, fn func(V) bool) map[K]map[K]V {
+	filtered := map[K]map[K]V{}
+
+	for k, v := range m {
 		for _, v1 := range v {
 			if fn(v1) {
-				filtered = append(filtered, v)
+				filtered[k] = v
 			}
 		}
 	}
@@ -117,13 +163,14 @@ func FilterMap[K comparable, V any](s []map[K]V, fn func(V) bool) []map[K]V {
 	return filtered
 }
 
-func Filter2DMap[K comparable, V any](s []map[K]map[K]V, fn func(map[K]V) bool) []map[K]map[K]V {
+// TODO consider to remove
+func Filter2DMapSlice[K comparable, V any](mapSlice []map[K]map[K]V, fn func(map[K]V) bool) []map[K]map[K]V {
 	filtered := []map[K]map[K]V{}
 
-	for _, v := range s {
-		for _, v1 := range v {
-			if fn(v1) {
-				filtered = append(filtered, v)
+	for _, s := range mapSlice {
+		for _, v := range s {
+			if fn(v) {
+				filtered = append(filtered, s)
 			}
 		}
 	}
