760022a193148a7055d9a748a3abbf1ee6e15b22
esimov <esimovaq@yahoo.com>
2022-06-09 14:49:19 +0300

    Moved Filter methods to filter file

--- cmd/filter.go
+++ cmd/filter.go
@@ -19,4 +19,51 @@ func main() {
 		return a > 10
 	})
 	fmt.Println(result2)
+
+	mp := map[int]string{1: "John", 2: "Doe", 3: "Fred"}
+
+	fmt.Println("==================FilterMap")
+	res2 := gogu.FilterMap[int, string](mp, func(v string) bool {
+		return v == "John"
+	})
+	fmt.Println(res2)
+
+	usersMap := map[string]map[string]int{
+		"bernie": {
+			"age":     30,
+			"ranking": 1,
+		},
+
+		"robert": {
+			"age":     20,
+			"ranking": 5,
+		},
+	}
+
+	fmt.Println("==================Filter2DMap")
+	res3 := gogu.Filter2DMap[string, int](usersMap, func(v int) bool {
+		return v > 20
+	})
+	fmt.Println(res3)
+
+	usersSlice := []map[string]map[string]int{
+		{
+			"bernie": {
+				"age":     30,
+				"ranking": 1,
+			},
+		},
+		{
+			"robert": {
+				"age":     20,
+				"ranking": 5,
+			},
+		},
+	}
+
+	fmt.Println("==================Filter2DMapSlice")
+	res4 := gogu.Filter2DMapSlice[string, int](usersSlice, func(v map[string]int) bool {
+		return v["age"] > 20 && v["ranking"] < 5
+	})
+	fmt.Println(res4)
 }
--- filter.go
+++ filter.go
@@ -11,3 +11,47 @@ func Filter[T any](s []T, fn func(T) bool) []T {
 
 	return rs
 }
+
+// FilterMap iterates over the elements of a collection and returns a new collection
+// representing all the items which satisfies the criteria formulated in the callback function.
+func FilterMap[K comparable, V any](m map[K]V, fn func(V) bool) map[K]V {
+	filtered := map[K]V{}
+
+	for k, v := range m {
+		if fn(v) {
+			filtered[k] = v
+		}
+	}
+
+	return filtered
+}
+
+// Filter2DMap is like FilterMap only applied on a two dimmensional map.
+func Filter2DMap[K comparable, V any](m map[K]map[K]V, fn func(V) bool) map[K]map[K]V {
+	filtered := map[K]map[K]V{}
+
+	for k, v := range m {
+		for _, v1 := range v {
+			if fn(v1) {
+				filtered[k] = v
+			}
+		}
+	}
+
+	return filtered
+}
+
+// TODO consider to remove
+func Filter2DMapSlice[K comparable, V any](sm []map[K]map[K]V, fn func(map[K]V) bool) []map[K]map[K]V {
+	filtered := []map[K]map[K]V{}
+
+	for _, s := range sm {
+		for _, v := range s {
+			if fn(v) {
+				filtered = append(filtered, s)
+			}
+		}
+	}
+
+	return filtered
+}
