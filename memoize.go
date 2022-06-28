f1ad355cea303b1c9623f28583cc220147db4f67
esimov <esimovaq@yahoo.com>
2022-06-28 17:15:58 +0300

    Fixed mutex deadlock issue

--- memoize.go
+++ memoize.go
@@ -43,45 +43,18 @@ func NewCache(d time.Duration) *Cache {
 }
 
 func (c *cache) Set(k string, val any, d time.Duration) error {
-	c.mu.Lock()
-
 	_, found := c.Get(k)
 	if found {
-		c.mu.Unlock()
-		return fmt.Errorf("Item with key %s already exists. Use Update method", k)
+		return fmt.Errorf("item with key %s already exists. Use Update method", k)
 	} else {
 		c.Add(k, val, d)
 	}
-	c.mu.Unlock()
 
 	return nil
 
 }
 
-func (c *cache) Add(k string, val any, d time.Duration) {
-	var ex int64
-	if d > 0 {
-		ex = time.Now().Add(d).UnixNano()
-	}
-
-	c.mu.Lock()
-
-	c.items[k] = &Item{
-		Object:     val,
-		Expiration: ex,
-	}
-
-	c.mu.Unlock()
-}
-
-func (c *cache) Get(k string) (*Item, bool) {
-	if _, ok := c.items[k]; ok {
-		return c.items[k], true
-	}
-	return &Item{}, false
-}
-
-func (c *cache) Update(k string, val any, d time.Duration) error {
+func (c *cache) Add(k string, val any, d time.Duration) error {
 	var ex int64
 	if d > 0 {
 		ex = time.Now().Add(d).UnixNano()
@@ -89,17 +62,40 @@ func (c *cache) Update(k string, val any, d time.Duration) error {
 
 	_, found := c.Get(k)
 	if found {
-		c.mu.Lock()
-		c.items[k] = &Item{
-			Object:     val,
-			Expiration: ex,
-		}
-		c.mu.Unlock()
-
-		return nil
+		return fmt.Errorf("item with key %s already exists", k)
 	}
 
-	return fmt.Errorf("Item with key %s does not exists", k)
+	c.mu.Lock()
+	c.items[k] = &Item{
+		Object:     val,
+		Expiration: ex,
+	}
+	c.mu.Unlock()
+
+	return nil
+}
+
+func (c *cache) Get(k string) (*Item, bool) {
+	c.mu.RLock()
+	if _, ok := c.items[k]; ok {
+		c.mu.RUnlock()
+		return c.items[k], true
+	}
+	c.mu.RUnlock()
+	return &Item{}, false
+}
+
+func (c *cache) Update(k string, val any, d time.Duration) error {
+	c.mu.Lock()
+	_, found := c.Get(k)
+	if !found {
+		c.mu.Unlock()
+		return fmt.Errorf("item with key %s does not exists", k)
+	}
+	c.Set(k, val, d)
+	c.mu.Unlock()
+	return nil
+
 }
 
 func (c *cache) SetDefault(k string, val any) {
