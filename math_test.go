8e529241ca0749f4347138ea3adbaf835580af31
esimov <esimovaq@yahoo.com>
2022-07-19 15:57:42 +0300

    Math test cases

---
+++ math_test.go
@@ -0,0 +1,19 @@
+package gogu
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMath(t *testing.T) {
+	assert := assert.New(t)
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	assert.Equal(1, Min(values...))
+	assert.Equal(10, Max(values...))
+	assert.Equal(2, Abs(-2))
+
+	assert.Equal(5, Clamp(10, 1, 5))
+	assert.Equal(-2, Clamp(-4, -2, 10))
+}
