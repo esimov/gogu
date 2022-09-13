545636d666b0a24dbc5cbadc4241a7f8431e432e
esimov <esimovaq@yahoo.com>
2022-09-13 12:08:07 +0300

    Extended linked list with Delete methods

--- list/slist.go
+++ list/slist.go
@@ -10,7 +10,7 @@ type node[T comparable] struct {
 	next *node[T]
 }
 
-// SList holds the nodes of the simple linked list.
+// SList holds the individual nodes of the list.
 type SList[T comparable] struct {
 	node[T]
 }
@@ -24,15 +24,15 @@ func newNode[T comparable](data T) *node[T] {
 	}
 }
 
-// InitList initializes a new single linked list with a single node
-// Since this is the only node its next pointer will be nil.
+// InitList initializes a new single linked list with one node.
+// Because this is the only node currently existing in the list its next pointer will be nil.
 func InitList[T comparable](data T) *SList[T] {
 	return &SList[T]{
 		*newNode(data),
 	}
 }
 
-// Unshift insert a new node at the beginning of the linked list.
+// Unshift insert a new node at the beginning of the list.
 func (l *SList[T]) Unshift(data T) {
 	node := newNode(data)
 
@@ -42,7 +42,7 @@ func (l *SList[T]) Unshift(data T) {
 }
 
 // InsertAfter inserts a new node after the provided node.
-// In case the node does not exists it returns an error.
+// In case the requested node is not in the list it returns an error.
 func (l *SList[T]) InsertAfter(prev *node[T], data T) error {
 	if prev == nil {
 		return fmt.Errorf("the provided node does not exists")
@@ -55,7 +55,62 @@ func (l *SList[T]) InsertAfter(prev *node[T], data T) error {
 	return nil
 }
 
-// Append inserts a new node at the end of the linked list.
+// Delete deletes the specified node from the list.
+func (l *SList[T]) Delete(n *node[T]) error {
+	tmp := &l.node
+	// Check if the node we want to delete is the first one.
+	if tmp.data == n.data {
+		l.node = *tmp.next
+		return nil
+	}
+
+	prev := node[T]{}
+	// Go through the list until the requested node is reached.
+	for tmp.next != nil && tmp.data != n.data {
+		prev = *tmp
+		tmp = tmp.next
+	}
+
+	// Check if the node we want to delete is the last one.
+	if tmp.next == nil {
+		l.DeleteLast()
+		return nil
+	}
+	*prev.next = *tmp.next
+
+	return nil
+}
+
+// DeleteFirst deletes the first node from the list.
+func (l *SList[T]) DeleteFirst() *node[T] {
+	firstNode := &l.node
+	node := l.node
+
+	if firstNode.next != nil {
+		firstNode = firstNode.next
+		l.node = *firstNode
+	}
+
+	return &node
+}
+
+// DeleteLast deletes the last node from the list.
+func (l *SList[T]) DeleteLast() *node[T] {
+	firstNode := l.node
+	tmp := &l.node
+	node := &node[T]{}
+
+	for tmp.next.next != nil {
+		node = tmp
+		tmp = tmp.next
+	}
+	tmp.next = nil
+	l.node = firstNode
+
+	return node
+}
+
+// Append inserts a new node at the end of the list.
 func (l *SList[T]) Append(data T) *node[T] {
 	node := newNode(data)
 	lastNode := &l.node
@@ -77,10 +132,11 @@ func (l *SList[T]) Append(data T) *node[T] {
 	return node
 }
 
-// Each iterates over the elements of the linked list and calls
+// Each iterates over the elements of the linked list and invokes
 // the callback function, having as parameter the nodes data.
 func (l *SList[T]) Each(fn func(data T)) {
 	node := &l.node
+	tmp := l.node
 	for {
 		fn(l.node.data)
 		if node.next == nil {
@@ -88,4 +144,6 @@ func (l *SList[T]) Each(fn func(data T)) {
 		}
 		l.node = *node.next
 	}
+	// Move the pointer back to the first node.
+	l.node = tmp
 }
