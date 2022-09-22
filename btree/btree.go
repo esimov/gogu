8d07830f5d2d906af19a58d4b918ba4fc25c68fb
esimov <esimovaq@yahoo.com>
2022-09-22 16:32:28 +0300

    Refactored B-tree implementation

--- btree/btree.go
+++ btree/btree.go
@@ -1,3 +1,9 @@
+// Package btree provides an implementation of the B-tree data structure,
+// which is a self-balancing tree data structure maintaining its values
+// in sorted order and allowing each node to have more than two children,
+// compared to the standard BST where each node has only two leaves.
+// The implementation is a simplified version of
+// https://algs4.cs.princeton.edu/62btree/BTree.java.
 package btree
 
 import (
@@ -8,83 +14,100 @@ import (
 // Max children per binary tree must be even or greater than 2.
 const maxChildren = 4
 
-type entry[T any] struct {
-	data T
-	next *node[T]
+// entry is the inner component of a node, which holds the node value and a pointer to the next node.
+type entry[K, V any] struct {
+	key       K
+	value     V
+	isRemoved bool
+	next      *node[K, V]
 }
 
-type node[T any] struct {
+// node is a data structure which defines how many children (leaves) each node has.
+type node[K, V any] struct {
 	m        int // number of children
-	children [maxChildren]entry[T]
+	children [maxChildren]entry[K, V]
 }
 
-func newNode[T any](m int) *node[T] {
-	return &node[T]{
+// newNode instantiates a new node with no leaves.
+func newNode[K, V any](m int) *node[K, V] {
+	return &node[K, V]{
 		m: m,
 	}
 }
 
-type BTree[T constraints.Ordered] struct {
-	n      int // the size of the binary tree (the number of nodes)
-	heigth int // the height of the binary tree
-	root   *node[T]
+// BTree is the main component of the B-tree which starts only with one node, which is the root.
+type BTree[K constraints.Ordered, V any] struct {
+	n      int // the size of the tree (the number of nodes)
+	heigth int // the height of the tree
+	root   *node[K, V]
 }
 
-func New[T constraints.Ordered]() *BTree[T] {
-	return &BTree[T]{
-		root: newNode[T](0),
+// New creates a new B-tree.
+func New[K constraints.Ordered, V any]() *BTree[K, V] {
+	return &BTree[K, V]{
+		root: newNode[K, V](0),
 	}
 }
 
-func (t *BTree[T]) Size() int {
+// Size returns the B-tree size (the number of elements).
+func (t *BTree[K, V]) Size() int {
 	return t.n
 }
 
-func (t *BTree[T]) IsEmpty() bool {
+// IsEmpty checks if a B-tree is empty or not.
+func (t *BTree[K, V]) IsEmpty() bool {
 	return t.Size() == 0
 }
 
-func (t *BTree[T]) Height() int {
+// Height returns the B-tree size (how many levels it has).
+func (t *BTree[K, V]) Height() int {
 	return t.heigth
 }
 
-func (t *BTree[T]) Get(val T) (T, bool) {
-	return t.search(t.root, val, t.heigth)
+// Get searches for a value in the tree and if it's found it returns the value
+// together with a boolean value signalig if it's found or not.
+func (t *BTree[K, V]) Get(key K) (V, bool) {
+	return t.search(t.root, key, t.heigth)
 }
 
-func (t *BTree[T]) search(n *node[T], data T, height int) (T, bool) {
-	// extenernal node
-	var val T
+// search is a private method which is invoked by the Get method.
+func (t *BTree[K, V]) search(n *node[K, V], key K, height int) (V, bool) {
+	// external node
 	if height == 0 {
 		for i := 0; i < n.m; i++ {
-			if gogu.Equal(data, n.children[i].data) {
-				return n.children[i].data, true
+			if gogu.Equal(key, n.children[i].key) {
+				return n.children[i].value, true
 			}
 		}
 	} else {
 		// internal node
 		for i := 0; i < n.m; i++ {
-			if i+1 == n.m || gogu.Less(data, n.children[i+1].data) {
-				return t.search(n.children[i].next, data, height-1)
+			if i+1 == n.m || gogu.Less(key, n.children[i+1].key) {
+				return t.search(n.children[i].next, key, height-1)
 			}
 		}
 	}
 
-	return val, false
+	var v V
+	return v, false
 }
 
-func (t *BTree[T]) Put(val T) {
-	u := t.insert(t.root, val, t.heigth)
+// Put inserts a new value into the B-tree.
+// If val is nil, this effectively deletes the value from the tree.
+func (t *BTree[K, V]) Put(key K, val V) {
+	u := t.insert(t.root, key, val, t.heigth, false)
 	t.n++
 	if u == nil {
 		return
 	}
 	// split the root
-	n := newNode[T](2)
-	n.children[0] = entry[T]{
+	n := newNode[K, V](2)
+	n.children[0] = entry[K, V]{
+		key:  t.root.children[0].key,
 		next: t.root,
 	}
-	n.children[1] = entry[T]{
+	n.children[1] = entry[K, V]{
+		key:  u.children[0].key,
 		next: u,
 	}
 
@@ -92,33 +115,37 @@ func (t *BTree[T]) Put(val T) {
 	t.heigth++
 }
 
-func (t *BTree[T]) insert(n *node[T], data T, height int) *node[T] {
-	entry := entry[T]{
-		data: data,
-		next: nil,
+// insert is a private method which is invoked by the Put method.
+func (t *BTree[K, V]) insert(n *node[K, V], key K, val V, height int, isRemoved bool) *node[K, V] {
+	entry := entry[K, V]{
+		key:   key,
+		value: val,
+		next:  nil,
 	}
 
 	var j int
 	// external node
 	if height == 0 {
 		for j = 0; j < n.m; j++ {
-			if gogu.Equal(data, n.children[j].data) {
-				var val T
-				n.children[j].data = val
+			// If the value already exists in the B-tree this will be overwritten.
+			if gogu.Equal(key, n.children[j].key) {
+				n.children[j].value = val
+				n.children[j].isRemoved = isRemoved
 				return nil
-			} else if gogu.Less(data, n.children[j].data) {
+			} else if gogu.Less(key, n.children[j].key) {
 				break
 			}
 		}
 	} else {
 		// internal node
 		for j = 0; j < n.m; j++ {
-			if j+1 == n.m || gogu.Less(data, n.children[j+1].data) {
-				node := t.insert(n.children[j].next, data, height-1)
+			if j+1 == n.m || gogu.Less(key, n.children[j+1].key) {
+				node := t.insert(n.children[j].next, key, val, height-1, isRemoved)
 				if node == nil {
 					return nil
 				}
 				j++
+				entry.key = node.children[0].key
 				entry.next = node
 				break
 			}
@@ -137,8 +164,8 @@ func (t *BTree[T]) insert(n *node[T], data T, height int) *node[T] {
 	}
 }
 
-func (t *BTree[T]) split(n *node[T]) *node[T] {
-	h := newNode[T](maxChildren / 2)
+func (t *BTree[K, V]) split(n *node[K, V]) *node[K, V] {
+	h := newNode[K, V](maxChildren / 2)
 	n.m = maxChildren / 2
 
 	for i := 0; i < n.m; i++ {
@@ -147,28 +174,31 @@ func (t *BTree[T]) split(n *node[T]) *node[T] {
 	return h
 }
 
-func (t *BTree[T]) Remove(val T) {
-	_, ok := t.Get(val)
+// Remove deletes an element from the B-tree.
+func (t *BTree[K, V]) Remove(key K) {
+	val, ok := t.Get(key)
 	if !ok {
 		return
 	}
-	t.insert(t.root, val, t.heigth)
 	t.n--
+	t.insert(t.root, key, val, t.heigth, true)
 }
 
-func (t *BTree[T]) Traverse(fn func(data T)) {
+// Traverse iterates over the values of the tree and invokes
+// the callback function provided as argument over the node elements.
+func (t *BTree[K, V]) Traverse(fn func(key K, val V)) {
 	t.traverse(t.root, t.heigth, fn)
 }
 
-func (t *BTree[T]) traverse(n *node[T], depth int, fn func(data T)) {
+func (t *BTree[K, V]) traverse(n *node[K, V], depth int, fn func(K, V)) {
 	// extenernal node
-	var val T
 	if depth == 0 {
 		for i := 0; i < n.m; i++ {
-			if gogu.Equal(val, n.children[i].data) {
+			l := n.children[i]
+			if l.isRemoved {
 				continue
 			}
-			fn(n.children[i].data)
+			fn(l.key, l.value)
 		}
 	} else {
 		// internal node
